8102a97513e7daa6a84ca18896d6fcbdc2e9dd47
DivakaraGitHub <dk00600384@techmahindra.com>
2020-04-25 23:28:42 +0530

    Update project

--- src/main/java/io/fabric8/quickstarts/camel/routes/RestRoute.java
+++ src/main/java/io/fabric8/quickstarts/camel/routes/RestRoute.java
@@ -3,20 +3,20 @@ package io.fabric8.quickstarts.camel.routes;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-import io.swagger.util.Json;
-
 @Component
 public class RestRoute extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-
-		rest("/{{api.ver}}/say").id("test-route1").description("Test Route POST API").consumes("application/json")
-				.produces("application/json").post("/bye").description("Post supplier purchase orders status")
-				.outType(Json.class).type(Json.class).to("direct:bye");
-
-		from("direct:bye").log("I am here").transform().constant("Bye World");
-
-	}
+		/*
+		 * 
+		 * rest("/{{api.ver}}/say").id("test-route1").description("Test Route POST API")
+		 * .consumes("application/json") .produces("application/json").post("/bye").
+		 * description("Post supplier purchase orders status")
+		 * .outType(Json.class).type(Json.class).to("direct:bye");
+		 * 
+		 * from("direct:bye").log("I am here").transform().constant("Bye World");
+		 * 
+		 */}
 
 }
