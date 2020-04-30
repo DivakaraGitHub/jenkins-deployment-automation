b37e630dd7829dcb6fca17849f689ce9495234fc
DivakaraGitHub <dk00600384@techmahindra.com>
2020-04-30 15:09:35 +0530

    Update project

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -18,10 +18,11 @@ package io.fabric8.quickstarts.camel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 // load regular Spring XML file from the classpath that contains the Camel XML DSL
-//@ImportResource({"classpath:spring/camel-context.xml"})
+@ImportResource({"classpath:spring/camel-context.xml"})
 public class Application {
 
     /**
